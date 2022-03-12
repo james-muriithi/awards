35175b13f6e9a5f414e3e2eaa081ed9fa83f8b6b
james-muriithi <muriithijames556@gmail.com>
2022-03-13 01:32:40 +0300

    updated user model fields

--- app/models.py
+++ app/models.py
@@ -1,12 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
-
+from cloudinary.models import CloudinaryField
 
 
 # Create your models here.
 class User(AbstractUser):
     full_name = models.CharField(max_length=124)
     email = models.CharField(max_length=124, unique=True)
+    avatar = CloudinaryField('image', null=True)
+    bio = models.TextField(max_length=500, blank=True, null=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'full_name']
