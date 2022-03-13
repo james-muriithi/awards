6b85322dcc9a4c01397796f6906585d94359c0be
james-muriithi <muriithijames556@gmail.com>
2022-03-13 23:56:58 +0300

    project url copy

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -10,4 +10,34 @@ $(function () {
     $('#imageUpload').on('change', function () {
         $(this).closest('form').trigger('submit')
     });
+
+    $('[data-bs-toggle="tooltip"]').tooltip()
+
+    $('.share').on('click', function () {
+        const url = $(this).data('url')
+        const title = $(this).attr('title')
+        if (navigator.share) {
+            navigator.share({
+                title,
+                text: url,
+                url
+            }).then(() => {
+                showCopiedTooltip($(this))
+            }).catch(err => {
+                console.log(`Couldn't share because of`, err.message);
+            });
+        } else {
+            navigator.clipboard.writeText(url);
+            showCopiedTooltip($(this))
+        }
+    })
 })
+
+const showCopiedTooltip = function (element, delay = 3000) {
+    element.attr('data-bs-original-title', 'Url copied!')
+    element.tooltip("show")
+    setTimeout(() => {
+        element.attr('data-bs-original-title', 'Copy image url')
+        element.tooltip("hide")
+    }, delay)
+}
